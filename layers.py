f592e94a13b8e05547636243587ca61c411644a1
slebras <slebras@stanford.edu>
2017-11-21 16:55:26 -0800

    updated the code. softcnn.py - changed the convolutions to be 1d and the input shape to be [batch size, 600,4] and the label shape to be [batch_size, num_labels(164), 2] (to have a one hot classification). also added batch normalization after every layer (conv1,conv2,conv3,fc1,fc2). still needs to be connected to the batch feeding mechanism. Also creates graphs of the following varibales for both train and test matched against epoch: accuracy, precision, recall, loss, msqe, and auc.

--- layers.py
+++ layers.py
@@ -6,6 +6,9 @@ def weight_variable(shape, name = ""):
 	return tf.get_variable(name+"_weight", shape = shape, 
 		initializer=tf.contrib.layers.xavier_initializer())
 
+def batch_norm(x, is_train):
+	return tf.contrib.layers.batch_norm(x, center=True, scale=True, is_training = is_train)
+
 
 def bias_variable(shape, name = ""):
 	return tf.get_variable(name+"_bias", shape = shape, 
@@ -15,19 +18,25 @@ def bias_variable(shape, name = ""):
 def conv2d(x,W, strides = [1,1,1,1], padding = "SAME"):
 	return tf.nn.conv2d(x,W,strides = strides, padding = padding)
 
+def conv1d(x,W,stride, padding = "SAME"):
+	return tf.nn.conv1d(x,W,stride = stride, padding = padding)
+
 
 def max_pool(x, size = 2, strides = [1,1,1,1], padding = 'SAME', name = ""):
 	with tf.variable_scope("max_pool%ix%i"%(size,size)):
 		return tf.nn.max_pool(x, ksize =[1,size,size,1], strides = strides, padding = padding)
 
+def max_pool1d(x, size, stride, padding = 'SAME', name = ""):
+	with tf.variable_scope("max_pool%i"%size):
+		return tf.nn.pool(A, [size], 'MAX', padding, strides = [stride])
 
 def avg_pool(x, size = 2, strides = [1,1,1,1], padding = 'SAME', name = ""):
 	with tf.variable_scope("avg_pool%ix%i"%(i,i)):
 		return tf.nn.avg_pool(x, ksize=[1,size,size,1], strides=strides, padding = padding, name = name)
 
 
-def convLayer(x, filterSize, outputDim, strides = [1,1,1,1], padding = 'SAME',name = ""):
-	with tf.variable_scope("conv"):
+def conv2dLayer(x, filterSize, outputDim, strides = [1,1,1,1], padding = 'SAME',name = ""):
+	with tf.variable_scope("conv2d"):
 		shape = x.get_shape().as_list()
 		inputDim = shape[3]
 
@@ -36,11 +45,24 @@ def convLayer(x, filterSize, outputDim, strides = [1,1,1,1], padding = 'SAME',na
 		conv = conv2d(x, W, strides, padding = padding)
 		return tf.nn.relu(tf.nn.bias_add(conv, b))
 
+def conv1dLayer(x,filterSize,outputDim,stride = 1, padding = "SAME", name = "")
+	with tf.variable_scope("conv1d"):
+		shape = x.get_shape().as_list()
+		inputDim = shape[2]
+
+		W = weight_variable([filterSize,inputDim,outputDim], name = name)
+		b = bias_variable([outputDim], name = name)
+		conv = conv2d(x, W, strides, padding = padding)
+		return tf.nn.relu(tf.nn.bias_add(conv, b))
+
+
 
 def denseLayer(x, outputDim, name = ""):
 	with tf.variable_scope("dense"):
 		shape = x.get_shape().as_list()
 		inputDim = None
+		if len(shape) == 3:
+			inputDim = shape[1]*shape[2]
 		if len(shape) == 4:
 			inputDim = shape[1]*shape[2]*shape[3]
 		if len(shape) == 2:
@@ -49,7 +71,7 @@ def denseLayer(x, outputDim, name = ""):
 
 		W = weight_variable([inputDim,outputDim], name = name)
 		b = bias_variable(outputDim, name = name)
-		if len(shape) == 4:
+		if len(shape) == 4 or len(shape) == 3:
 			x = tf.reshape(x, reshape)
 		return tf.nn.relu(tf.nn.bias_add(tf.matmul(x, W), b))
 
--- softcnn.py
+++ softcnn.py
@@ -1,10 +1,17 @@
 import tensorflow as tf
 import layers
 import utils
+import seaborn as sns
+import matplotlib
+matplotlib.use("Agg")
+from matplotlib import plt
 
 
 class softcnn():
 
+	'''
+	initialiations
+	'''
 	def __init__(self, train_path, test_path, cell_types, batch_size = 50, num_epochs = 100,  input_shape = [600,4], num_labels = 164,
 		logdir = "", , learning_rate = 0.002, write_to_log_frequency = 100, weight_decay = 0.01):
 		self.input_shape = input_shape
@@ -20,31 +27,40 @@ class softcnn():
 		self.weight_decay = weight_decay
 		self.write_to_log_frequency = write_to_log_frequency
 
+		#sets up the new tf session
 		tf.reset_default_graph()		
 		self.sess = tf.Session()
 		
+		#sets up the graph of the network
 		with tf.variable_scope("soft_cnn"):
 			self.make_graph()
 
+		#sets up the writer for tensorboard functionality
 		if not os.path.isdir(logdir):
 			os.mkdir(logdir)
 		self.writer = tf.summary.FileWriter(logdir,self.sess.graph)
 		self.sess.run(tf.global_variables_initializer())
 
 	def make_graph(self):
-		self.x = tf.placeholder(tf.float32, shape=[self.batch_size,self.input_shape[0], self.input_shape[1], 1])
-		self.lbls = tf.placeholder(tf.float32, shape=[self.batch_size, self.num_labels])
-		self.accuracies, self.precision, self.auces, self.msqes, self.losses, self.recalls, self.precisions = [],[],[],[],[],[],[]
+		self.x = tf.placeholder(tf.float32, shape=[self.batch_size,self.input_shape[0], self.input_shape[1]])
+		self.lbls = tf.placeholder(tf.float32, shape=[self.batch_size, self.num_labels, 2])
+		self.is_train = tf.placeholder(tf.bool)
+		self.accuracies, self.auces, self.msqes, self.losses, self.recalls, self.precisions = [],[],[],[],[],[]
 		self.total_loss = None
 		for i in range(self.num_labels):
 			with tf.variable_scope(self.cell_types[i]):
-				acc, loss, recall, precision, auc, msqe = self.make_single_graph(self.x, self.lbls, 1)
+				lbl = self.lbls[:,i,:]
+				lbl = tf.reshape(lbl,[self.batch_size, 2])
+				acc, loss, recall, precision, auc, msqe = self.make_single_graph(self.x, lbl)
 
 
 			self.losses.append(loss)
 			self.accuracies.append(acc)
 			self.msqes.append(msqe)
 			self.auces.append(auc)
+			self.precisions.append(precision)
+			self.recalls.append(recall)
+
 			self.total_loss += loss
 
 		self.accuracy = tf.divide(tf.add_n(self.accuracies), self.num_labels)
@@ -74,25 +90,28 @@ class softcnn():
 		self.train_auc_op = tf.summary.scalar("train_auc", self.auc)
 		self.test_auc_op = tf.summary.scalar("test_auc", self.auc)
 
-	def make_single_graph(self, x, lbls, number_of_final_inputs):
+	def make_single_graph(self, x, lbls):
 
 
-		conv1 =  layers.max_pool(
-					layers.convLayer(x, filterSize = [19,4], outputDim = 300, strides = [1,1,1,1], name = "conv1"),
-					, size = 4, strides = [1,4,4,1], name = "pool1")
+		conv1 =  layers.max_pool1d(
+					layers.batch_norm(
+						layers.conv1dLayer(x, filterSize = 19, outputDim = 300, stride = 1, name = "conv1"),
+					self.is_train) , size = 3, stride = 3, name = "pool1")
 
-		conv2 =  layers.max_pool(
-					layers.convLayer(self.conv1, filterSize = [11,4], outputDim = 200, strides = [1,1,1,1], name = "conv2")
-					, size = 4, strides = [1,4,4,1], name = "pool2")
+		conv2 =  layers.max_pool1d(
+					layers.batch_norm(
+						layers.conv1dLayer(self.conv1, filterSize = 11, outputDim = 200, stride =1, name = "conv2"),
+					self.is_train), size = 4, stride = 4, name = "pool2")
 
-		conv3 =  layers.max_pool(
-					layers.convLayer(self.conv2, filterSize = [7,4], outputDim = 200, strides = [1,1,1,1], name = "conv3")
-					, size = 4, strides = [1,4,4,1], name = "pool3")
+		conv3 =  layers.max_pool1d(
+					layers.batch_norm(
+						layers.conv1dLayer(self.conv2, filterSize = 7, outputDim = 200, stride = 1, name = "conv3"),
+					self.is_train), size = 4, stride = 4, name = "pool3")
 
-		fc1   = layers.denseLayer(self.conv3, outputDim = 1000, name = "fc1")
-		fc2   = layers.denseLayer(self.fc1, outputDim = 1000, name = "fc2")
+		fc1   = layers.batch_norm(layers.denseLayer(self.conv3, outputDim = 1000, name = "fc1"), self.is_train)
+		fc2   = layers.batch_norm(layers.denseLayer(self.fc1, outputDim = 1000, name = "fc2"), self.is_train)
 
-		y_conv = layers.readoutLayer(self.fc2, outputDim = number_of_final_inputs, name = "readout")
+		y_conv = layers.readoutLayer(self.fc2, outputDim = 2, name = "readout")
 		loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = lbls, logits = y_conv))
 		# train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(loss)
 
@@ -119,29 +138,29 @@ class softcnn():
 
 	def run_iter(self, iter_input, iter_lbls, to_log, is_train = True):
 		if is_train:
-			self.sess.run(self.train_step, self.x = , self.lbls =  )
+			self.sess.run(self.train_step, feed_dict={self.x = iter_input, self.lbls = iter_lbls, self.is_train = is_train})
 			self.curr_step += 1
 
-		acc = self.sess.run(self.accuracy, feed_dict={self.x = iter_input, self.lbls = iter_lbls})
-		loss = self.sess.run(self.total_loss, feed_dict=(self.x = iter_input, self.lbls = iter_lbls))
-		recall = self.sess.run(self.recall, feed_dict=(self.x = iter_input, self.lbls = iter_lbls))
-		precision = self.sess.run(self.precision, feed_dict=(self.x = iter_input, self.lbls = iter_lbls))
-		auc = self.sess.run(self.auc, feed_dict=(self.x = iter_input, self.lbls = iter_lbls))
-		msqe = self.sess.run(self.msqe, feed_dict=(self.x = iter_input, self.lbls = iter_lbls))
+		acc = self.sess.run(self.accuracy, feed_dict={self.x = iter_input, self.lbls = iter_lbls, self.is_train = is_train})
+		loss = self.sess.run(self.total_loss, feed_dict={self.x = iter_input, self.lbls = iter_lbls, self.is_train = is_train})
+		recall = self.sess.run(self.recall, feed_dict={self.x = iter_input, self.lbls = iter_lbls, self.is_train = is_train})
+		precision = self.sess.run(self.precision, feed_dict={self.x = iter_input, self.lbls = iter_lbls, self.is_train = is_train})
+		auc = self.sess.run(self.auc, feed_dict={self.x = iter_input, self.lbls = iter_lbls, self.is_train = is_train})
+		msqe = self.sess.run(self.msqe, feed_dict={self.x = iter_input, self.lbls = iter_lbls, self.is_train = is_train})
 
 		if to_log:
 			if is_train:
-				loss_str = self.sess.run(self.train_loss_op, feed_dict=(self.x = iter_input, self.lbls = iter_lbls))
-				acc_str = self.sess.run(self.train_acc_op, feed_dict=(self.x = iter_input, self.lbls = iter_lbls)) 
-				prec_str = self.sess.run(self.train_prec_op , feed_dict=(self.x = iter_input, self.lbls = iter_lbls))
-				recall_str = self.sess.run(self.train_recall_op, feed_dict=(self.x = iter_input, self.lbls = iter_lbls))
-				auc_str = self.sess.run(self.train_auc_op, feed_dict=(self.x = iter_input, self.lbls = iter_lbls))
+				loss_str = self.sess.run(self.train_loss_op, feed_dict={self.x = iter_input, self.lbls = iter_lbls, self.is_train = is_train})
+				acc_str = self.sess.run(self.train_acc_op, feed_dict={self.x = iter_input, self.lbls = iter_lbls, self.is_train = is_train}) 
+				prec_str = self.sess.run(self.train_prec_op , feed_dict={self.x = iter_input, self.lbls = iter_lbls, self.is_train = is_train})
+				recall_str = self.sess.run(self.train_recall_op, feed_dict={self.x = iter_input, self.lbls = iter_lbls, self.is_train = is_train})
+				auc_str = self.sess.run(self.train_auc_op, feed_dict={self.x = iter_input, self.lbls = iter_lbls, self.is_train = is_train})
 			else:
-				loss_str = self.sess.run(self.test_loss_op, feed_dict=(self.x = iter_input, self.lbls = iter_lbls))
-				acc_str = self.sess.run(self.test_acc_op , feed_dict=(self.x = iter_input, self.lbls = iter_lbls))
-				prec_str = self.sess.run(self.test_prec_op, feed_dict=(self.x = iter_input, self.lbls = iter_lbls))
-				recall_str = self.sess.run(self.test_recall_op, feed_dict=(self.x = iter_input, self.lbls = iter_lbls))
-				auc_str = self.sess.run(self.test_auc_op, feed_dict=(self.x = iter_input, self.lbls = iter_lbls))
+				loss_str = self.sess.run(self.test_loss_op, feed_dict={self.x = iter_input, self.lbls = iter_lbls, self.is_train = is_train})
+				acc_str = self.sess.run(self.test_acc_op , feed_dict={self.x = iter_input, self.lbls = iter_lbls, self.is_train = is_train})
+				prec_str = self.sess.run(self.test_prec_op, feed_dict={self.x = iter_input, self.lbls = iter_lbls, self.is_train = is_train})
+				recall_str = self.sess.run(self.test_recall_op, feed_dict={self.x = iter_input, self.lbls = iter_lbls, self.is_train = is_train})
+				auc_str = self.sess.run(self.test_auc_op, feed_dict={self.x = iter_input, self.lbls = iter_lbls, self.is_train = is_train})
 			self.writer.add_summary(acc_str, self.curr_step)
 			self.writer.add_summary(loss_str, self.curr_step)
 			self.writer.add_summary(prec_str, self.curr_step)
@@ -178,15 +197,37 @@ class softcnn():
 		avg_auc = float(sum(auces))/float(max(len(auces),1))
 		avg_msqe = float(sum(msqes))/float(max(len(msqes),1))
 
+
 		return avg_acc, avg_loss, avg_recall, avg_precision, avg_auc, avg_msqe
+
+	def printGraph(train, test, name):
+		sns.set_style("darkgrid")
+		epochs = [i+1 for i in range(numberOfEpochs)]
+		plt.plot(epochs, test, 'r', label = "Test")
+		plt.plot(epochs, train, 'b', label = "Train")
+		plt.legend(loc = "upper left")	
+		plt.title(name+" vs. Epochs")
+		plt.xlabel("Epochs")
+		plt.ylabel(name)
+		plt.savefig(".png")
+		plt.clf()
 
 
 	#CALL this Function is you want to run however many epochs you sent to the model
 	def fit(self):
+		train_acc, train_loss, train_recall, train_precision, train_auc, train_msqe = [],[],[],[],[],[]
+		test_acc, test_loss, test_recall, test_precision, test_auc, test_msqe = [],[],[],[],[],[]
+
 		for i in range(num_epochs):
 			self.curr_epoch += 1
 			print("-----STARTING EPOCH %i-----"%self.curr_epoch)
 			acc, loss, recall, precision, auc, msqe = self.run_epoch(is_train = True)
+			train_acc.append(acc)
+			train_loss.append(loss)
+			train_recall.append(recall)
+			train_precision.append(precision)
+			train_auc.append(auc)
+			train_msqe.append(msqe)
 			print("-Train: ")
 			print("----Accuracy: %.3f"%acc)
 			print("----Loss: %.3f"%loss)
@@ -195,6 +236,12 @@ class softcnn():
 			print("----AUC: %.3f"%auc)
 			print("----MSQE: %0.3f"%msqe)
 			acc, loss, recall, precision, auc, msqe = self.run_epoch(is_train = False)
+			test_acc.append(acc)
+			test_loss.append(loss)
+			test_recall.append(recall)
+			test_precision.append(precision)
+			test_auc.append(auc)
+			test_msqe.append(msqe)
 			print("-Test: ")
 			print("----Accuracy: %.3f"%acc)
 			print("----Loss: %.3f"%loss)
@@ -203,3 +250,11 @@ class softcnn():
 			print("----AUC: %.3f"%auc)
 			print("----MSQE: %0.3f"%msqe)
 			print("---------------------------")
+
+		printGraph(train_acc, test_acc, "Accuracy")
+		printGraph(train_loss, test_loss, "Loss")
+		printGraph(train_recall, test_recall, "Recall")
+		printGraph(train_precision, test_precision, "Precision")
+		printGraph(train_auc, test_auc, "Area Under ROC Curve")
+		printGraph(train_msqe, test_msqe, "Mean Squared Error")
+
