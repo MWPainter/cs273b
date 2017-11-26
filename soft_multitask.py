4eb55ed39b289a4dc7904d56a30e422d718839c1
aistrate1 <aistrate@stanford.edu>
2017-11-26 09:42:37 -0500

    Updated the hard_multitask model. Added a new implementation for soft_multitask.py

--- soft_multitask.py
+++ soft_multitask.py
@@ -17,6 +17,8 @@ TRAIN_PATH = 'logs/train/'
 MODEL_PATH = 'logs/model'
 N_TRAIN_STEPS = 1
 NUM_EPOCHS = 100
+NUM_LABELS = 164
+total_acc, total_recall, total_precision, total_auc_score, total_msqe = 0, 0, 0, 0, 0
 
 def model(lr = LEARNING_RATE):
     network = input_data(shape=[None, 600, 4], name='features')
@@ -28,7 +30,7 @@ def model(lr = LEARNING_RATE):
     network = max_pool_1d(network, 4, strides=4)
     network = fully_connected(network, 1000, activation='relu')
     network = fully_connected(network, 1000, activation='relu')
-    network = fully_connected(network, 164, activation='sigmoid')
+    network = fully_connected(network, 1, activation='sigmoid')
     network = regression(network, optimizer='rmsprop', loss='binary_crossentropy', 
                          learning_rate=lr, name='labels')
 
@@ -37,43 +39,38 @@ def model(lr = LEARNING_RATE):
 
     return model
 
-def get_batch(batch_size, is_train):
+def get_batch(batch_size, is_train, i):
         inputs, labels = [], []
         if is_train:
             features = np.load("/mnt/data/train_x.npy")
-            labels = np.load("/mnt/data/train_y.npy")
+            labels = np.load("/mnt/data/train_y.npy")[:,i]
         else:
             features = np.load("/mnt/data/val_x.npy")
-            labels = np.load("/mnt/data/val_y.npy")
+            labels = np.load("/mnt/data/val_y.npy")[:,i]
         indices = np.random.choice(np.arange(len(features)), batch_size, replace=False)
         inputs = features[indices]
         labels = labels[indices]
 
-        return inputs, labels
+        return inputs, np.reshape(labels, (batch_size, 1))
 
 def get_metrics(predictions, labels, y_conv):
 	acc, recall, precision, auc_score, msqe = 0, 0, 0, 0, 0
-	for i in range(164):
-		acc += accuracy_score(predictions[:,i], labels[:,i])
-		recall += recall_score(predictions[:,i], labels[:,i])
-		precision += precision_score(predictions[:,i], labels[:,i])
-		try:
-			auc_score += roc_auc_score(predictions[:,i], labels[:,i])
-		except ValueError:
-			pass
+	acc = accuracy_score(predictions[:,i], labels[:,i])
+	recall = recall_score(predictions[:,i], labels[:,i])
+	precision = precision_score(predictions[:,i], labels[:,i])
+	try:
+		auc_score += roc_auc_score(predictions[:,i], labels[:,i])
+	except ValueError:
+		pass
 
 	msqe = mean_squared_error(labels, y_conv)
-	auc_score  /= 164
-	acc  /= 164
-	precision  /= 164
-	recall /= 164
 	return acc, recall, precision, auc_score, msqe
 
-def train_conv_net(model, model_name):
+def train_conv_net(model, model_name, index):
     for i in range(N_TRAIN_STEPS):
         
-        x_train, y_train = get_batch(int(512), True) 
-        x_val_set, y_val_set = get_batch(int(124), False)            
+        x_train, y_train = get_batch(int(512), True, index) 
+        x_val_set, y_val_set = get_batch(int(124), False, index)            
         model.fit(x_train, y_train, n_epoch=NUM_EPOCHS, batch_size=256, validation_set=(x_val_set, y_val_set), show_metric=True, shuffle=True)
         print(metrics)
         predictions = model.predict(x_val_set)
@@ -86,8 +83,25 @@ def train_conv_net(model, model_name):
         print(recall)
         print(precision)
         print(auc_score)     
+        total_acc += acc
+        total_recall += recall
+        total_precision += precision
+        total_auc_score += auc_score
+        total_msqe += msqe  
 
-graph = tf.Graph()
-with graph.as_default():
-    train_model = model()
-    train_conv_net(train_model, MODEL_NAME)
+for i in range(NUM_LABELS):
+    print("Cell " + str(i))
+    graph = tf.Graph()
+    with graph.as_default():
+        train_model = model()
+        train_conv_net(train_model, MODEL_NAME, i)
+total_acc /= NUM_LABELS
+total_recall /= NUM_LABELS
+total_precision /= NUM_LABELS
+total_auc_score /= NUM_LABELS
+total_msqe /= NUM_LABELS
+print(total_acc) 
+print(total_msqe)
+print(total_recall)
+print(total_precision)
+print(total_auc_score)
