7d144d644b6fdac06cb443c272974189609af040
MWPainter <michaelpainter.1994@gmail.com>
2017-12-16 08:42:31 +0000

    Final commit

--- eval.py
+++ eval.py
@@ -1,3 +1,7 @@
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 from sklearn import metrics
 
@@ -18,8 +22,30 @@ def eval(ground_truths, prediction_scores, dir_to_print):
 
     max_accuracy = find_max_accuracy(ground_truths, prediction_scores, pr_thresholds)
 
+    printGraph(recalls, precisions, "precision vs recall", "recall", "precision", dir_to_print)
+    printGraph(fpr, tpr, "ROC curve", "False Positive Rate", "True Positive Rate", dir_to_print)
+
+    print "Max Accuracy: " + max_accuracy
+    print "ROC AUC: " + roc_area
+    print "PR AUC: " + precision_recall_area
+
 
 
 def find_max_accuracy(ground_truths, prediction_scores, pr_thresholds):
+    mx = 0.0
+    for thresh in pr_thresholds:
+        acc = metrics.accuracy_score(ground_truths, prediction_scores > thresh)
+        if acc > mx: mx = acc
+    return mx
 
 
+
+def printGraph(xs, ys, graph_name, xlbl, ylbl, dir_to_print):
+    sns.set_style("darkgrid")
+    plt.plot(xs, ys, 'r', label = "Test")
+    plt.legend(loc = "upper left")    
+    plt.title(graph_name)
+    plt.xlabel(xlbl)
+    plt.ylabel(ylbl)
+    plt.savefig(dir_to_print + graph_name)
+    plt.clf()
--- eval_model.py
+++ eval_model.py
@@ -1,6 +1,6 @@
-
-
-
+import numpy as np
+import tensorflow as tf
+from softcnn import softcnn
 
 
 
@@ -8,6 +8,17 @@
 
 
 # Load in the model
+run_name = "soft_small_set_0"
+data_path = "/mnt/data/"
+basedir = "/mnt/" + run_name + "/"
+logdir = basedir + "logs/"
+model = softcnn(run_name, data_path, batch_size = 50, num_epochs = 100, weight_decay = 0.01, basedir = basedir, logdir = logdir)
+model.make_graph()
+saver = tf.train.Saver()
+with tf.Session() as sess:
+    saver.restore(sess, "/mnt/soft_small_set_0/checkpoints/soft_small_set_0_epoch_15_step_13500.ckpt")
+    for v in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES):
+        print v
 
 
 
